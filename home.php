4b9d4e2d6c0e5caff96230a8f22c5fc6ce476ec4
abcsds <abcsds@gmail.com>
2015-08-12 16:06:20 -0500

    Add slider to home

--- home.php
+++ home.php
@@ -1,28 +1,32 @@
 <?php get_header(); ?>
 
-		
+
 		<div class="column-one">
-			
+
 			<h1>
 				<?php if (is_category()) { ?>
-						<?php _e("Posts Categorized", "gpp_theme"); ?> / <span><?php single_cat_title(); ?></span> 
-				<?php } elseif (is_tag()) { ?> 
+						<?php _e("Posts Categorized", "gpp_theme"); ?> / <span><?php single_cat_title(); ?></span>
+				<?php } elseif (is_tag()) { ?>
 						<?php _e("Posts Tagged", "gpp_theme"); ?> / <span><?php single_cat_title(); ?></span>
 				<?php } elseif (is_author()) { ?>
-						<?php _e("Posts By", "gpp_theme"); ?> / <span><?php the_author_meta('display_name', $post->post_author) ?> </span> 
+						<?php _e("Posts By", "gpp_theme"); ?> / <span><?php the_author_meta('display_name', $post->post_author) ?> </span>
 				<?php } elseif (is_day()) { ?>
 						<?php _e("Daily Archives", "gpp_theme"); ?> / <span><?php the_time('l, F j, Y'); ?></span>
 				<?php } elseif (is_month()) { ?>
 				    	<?php _e("Monthly Archives", "gpp_theme"); ?> / <span><?php the_time('F Y'); ?></span>
 				<?php } elseif (is_year()) { ?>
-				    	<?php _e("Yearly Archives", "gpp_theme"); ?> / <span><?php the_time('Y'); ?></span> 
+				    	<?php _e("Yearly Archives", "gpp_theme"); ?> / <span><?php the_time('Y'); ?></span>
 				<?php } elseif (is_Search()) { ?>
-				    	<?php _e("Search Results", "gpp_theme"); ?> / <span><?php echo esc_attr(get_search_query()); ?></span> 
+				    	<?php _e("Search Results", "gpp_theme"); ?> / <span><?php echo esc_attr(get_search_query()); ?></span>
 				<?php } ?>
 			</h1>
 
+			<div class="slider">
+				<?php echo do_shortcode("[huge_it_slider id='2']"); ?>
+			</div>
+
 			<div id="posts" class="clearfix">
-			
+
 				<?php
 				// WP 3.0 PAGED BUG FIX
 				if ( get_query_var('paged') )
@@ -34,7 +38,7 @@
 
 				$args = array(
 				'post_type' => 'post',
-				'paged' => $paged, 
+				'paged' => $paged,
 				'posts_per_page' => of_get_option('homepage_num_posts')
 				);
 
@@ -57,8 +61,8 @@
 
 
 			<!-- begin #pagination -->
-			<?php if (function_exists("emm_paginate")) { 
-				emm_paginate();  
+			<?php if (function_exists("emm_paginate")) {
+				emm_paginate();
 			} else { ?>
 			<div class="navigation">
 			     <div class="alignleft"><?php next_posts_link('Older') ?></div>
